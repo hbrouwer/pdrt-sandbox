73a6549002af3e72950cb77d0205259441774fc2
Harm Brouwer <me@hbrouwer.eu>
2013-10-17 16:15:10 +0200

    finished projectionTable

--- src/Data/PDRS/ProjectionTable.hs
+++ src/Data/PDRS/ProjectionTable.hs
@@ -33,7 +33,7 @@ instance Show PTable where
 data PTable = PTable [Item]
   deriving (Eq)
 
-type Item = (Content,PVar,[PVar])
+type Item = (Content,PVar,PVar)
 
 data Content =
   Ref PDRSRef
@@ -57,17 +57,17 @@ pdrsToItems (PDRS l _ u c) = universeToItems u l ++ pconsToItems c l
 
 universeToItems :: [PRef] -> PVar -> [Item]
 universeToItems [] _                         = []
-universeToItems (PRef p r:prs) l = (Ref r,l,[p]) : universeToItems prs l
+universeToItems (PRef p r:prs) l = (Ref r,l,p) : universeToItems prs l
 
 pconsToItems :: [PCon] -> PVar -> [Item]
 pconsToItems [] _ = []
-pconsToItems (PCon p (PDRS.Rel r d):pcs)    l = (Rel r d,l,[p])                : pconsToItems pcs l
-pconsToItems (PCon p (PDRS.Neg p1):pcs)     l = (Neg     (pdrsLabel p1),l,[p]) : pdrsToItems p1     ++ pconsToItems pcs l
-pconsToItems (PCon p (PDRS.Imp p1 p2):pcs)  l = (Imp     (pdrsLabel p1) (pdrsLabel p2),l,[p]) : pdrsToItems p1 ++ pdrsToItems p2 ++ pconsToItems pcs l
-pconsToItems (PCon p (PDRS.Or p1 p2):pcs)   l = (Or      (pdrsLabel p1) (pdrsLabel p2),l,[p]) : pdrsToItems p1 ++ pdrsToItems p2 ++ pconsToItems pcs l
-pconsToItems (PCon p (PDRS.Prop r p1):pcs)  l = (Prop r  (pdrsLabel p1),l,[p]) : pdrsToItems p1     ++ pconsToItems pcs l
-pconsToItems (PCon p (PDRS.Diamond p1):pcs) l = (Diamond (pdrsLabel p1),l,[p]) : pdrsToItems p1     ++ pconsToItems pcs l
-pconsToItems (PCon p (PDRS.Box p1):pcs)     l = (Box     (pdrsLabel p1),l,[p]) : pdrsToItems p1     ++ pconsToItems pcs l
+pconsToItems (PCon p (PDRS.Rel r d):pcs)    l = (Rel r d,l,p)                : pconsToItems pcs l
+pconsToItems (PCon p (PDRS.Neg p1):pcs)     l = (Neg     (pdrsLabel p1),l,p) : pdrsToItems p1     ++ pconsToItems pcs l
+pconsToItems (PCon p (PDRS.Imp p1 p2):pcs)  l = (Imp     (pdrsLabel p1) (pdrsLabel p2),l,p) : pdrsToItems p1 ++ pdrsToItems p2 ++ pconsToItems pcs l
+pconsToItems (PCon p (PDRS.Or p1 p2):pcs)   l = (Or      (pdrsLabel p1) (pdrsLabel p2),l,p) : pdrsToItems p1 ++ pdrsToItems p2 ++ pconsToItems pcs l
+pconsToItems (PCon p (PDRS.Prop r p1):pcs)  l = (Prop r  (pdrsLabel p1),l,p) : pdrsToItems p1     ++ pconsToItems pcs l
+pconsToItems (PCon p (PDRS.Diamond p1):pcs) l = (Diamond (pdrsLabel p1),l,p) : pdrsToItems p1     ++ pconsToItems pcs l
+pconsToItems (PCon p (PDRS.Box p1):pcs)     l = (Box     (pdrsLabel p1),l,p) : pdrsToItems p1     ++ pconsToItems pcs l
 
 showPTable :: PTable -> String
 showPTable (PTable is) = "[Type]\t[Content]\t[Intro st]\t[Proj. st]\n" ++ concatMap showItem is
@@ -75,15 +75,16 @@ showPTable (PTable is) = "[Type]\t[Content]\t[Intro st]\t[Proj. st]\n" ++ concat
 showItem :: Item -> String
 showItem (c,is,ps) =
   case c of
-    (Ref r)       -> "Ref" ++ "\t" ++ showRef r ++ "\t\t" ++ show is ++ "\t\t" ++ intercalate "," (map show ps) ++ "\n"
-    (Rel r d)     -> "Con" ++ "\t" ++ r ++ "("  ++ intercalate "," (map showRef d) ++ ")" ++ "\t\t"  ++ show is ++ "\t\t" ++ intercalate "," (map show ps) ++ "\n"
-    (Neg pv)      -> "Con" ++ "\t" ++ opNeg     ++ show pv ++ "\t\t" ++ show is ++ "\t\t" ++ intercalate "," (map show ps) ++ "\n"
-    (Imp pv1 pv2) -> "Con" ++ "\t" ++ show pv1  ++ " " ++ opImp ++ " " ++ show pv2 ++ "\t\t" ++ show is ++ "\t\t" ++ intercalate "," (map show ps) ++ "\n"
-    (Or pv1 pv2)  -> "Con" ++ "\t" ++ show pv1  ++ " " ++ opOr  ++ " " ++ show pv2 ++ "\t\t" ++ show is ++ "\t\t" ++ intercalate "," (map show ps) ++ "\n"
-    (Prop r pv)   -> "Con" ++ "\t" ++ showRef r ++ ":" ++ show pv ++ "\t\t" ++ show is ++ "\t\t" ++ intercalate "," (map show ps) ++ "\n"
-    (Diamond pv)  -> "Con" ++ "\t" ++ opDiamond ++ show pv ++ "\t\t" ++ show is ++ "\t\t" ++ intercalate "," (map show ps) ++ "\n"
-    (Box pv)      -> "Con" ++ "\t" ++ opBox     ++ show pv ++ "\t\t" ++ show is ++ "\t\t" ++ intercalate "," (map show ps) ++ "\n"
-  where showRef :: PDRSRef -> DRSVar
+    (Ref r)       -> "Ref" ++ "\t" ++ showRef r ++ tail
+    (Rel r d)     -> "Con" ++ "\t" ++ r ++ "("  ++ intercalate "," (map showRef d) ++ ")" ++ tail
+    (Neg pv)      -> "Con" ++ "\t" ++ opNeg     ++ " " ++ show pv ++ tail
+    (Imp pv1 pv2) -> "Con" ++ "\t" ++ show pv1  ++ " " ++ opImp ++ " " ++ show pv2 ++ tail
+    (Or pv1 pv2)  -> "Con" ++ "\t" ++ show pv1  ++ " " ++ opOr  ++ " " ++ show pv2 ++ tail
+    (Prop r pv)   -> "Con" ++ "\t" ++ showRef r ++ ":" ++ show pv ++ tail
+    (Diamond pv)  -> "Con" ++ "\t" ++ opDiamond ++ " " ++ show pv ++ tail
+    (Box pv)      -> "Con" ++ "\t" ++ opBox     ++ " " ++ show pv ++ tail
+  where tail = "\t\t" ++ show is ++ "\t\t" ++ show ps ++ "\n"
+        showRef :: PDRSRef -> DRSVar
         showRef = drsRefToDRSVar . pdrsRefToDRSRef
 
 printPTable :: PTable -> IO ()
