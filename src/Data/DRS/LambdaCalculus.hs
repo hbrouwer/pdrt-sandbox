f798d86fa184298daa8a08f47f4c509143f5af84
Noortje Venhuizen <njvenhuizen@gmail.com>
2013-10-23 13:15:05 +0200

    moved drsToCleanDRS to LambdaCalculus + changed name into drsPurify, turned drsDisjoin into sub-function of merge (in line with Data.PDRS)

--- src/Data/DRS/LambdaCalculus.hs
+++ src/Data/DRS/LambdaCalculus.hs
@@ -21,10 +21,13 @@ module Data.DRS.LambdaCalculus
 , (<<@>>)
 , drsFunctionCompose
 , (<<.>>)
+, drsPurify
 ) where
 
 import Data.DRS.Structure
 import Data.DRS.Variables
+
+import Data.List (intersect, union)
 
 -- | Applies alpha conversion to a DRS on the basis of a conversion list
 -- for DRS referents @rs@
@@ -100,3 +103,45 @@ drsFunctionCompose = (.)
 -- | Infix notation for 'drsFunctionCompose'
 (<<.>>) :: (b -> c) -> (a -> b) -> a -> c
 a1 <<.>> a2 = a1 `drsFunctionCompose` a2
+
+
+drsPurify :: DRS -> DRS
+drsPurify gdrs = fst $ purifyRefs (gdrs,[]) gdrs
+
+purifyRefs :: (DRS,[DRSRef]) -> DRS -> (DRS,[DRSRef])
+purifyRefs (ld@(LambdaDRS _),rs) _  = (ld,rs)
+purifyRefs (Merge d1 d2,rs)      gd = (Merge cd1 cd2,rs2)
+  where (cd1,rs1) = purifyRefs (d1,rs)  gd
+        (cd2,rs2) = purifyRefs (d2,rs1) gd
+purifyRefs (d@(DRS u _),rs)      gd = (DRS u1 c2,rs1)
+  where (DRS u1 c1) = drsAlphaConvert d (zip ors (newDRSRefs ors (drsVariables gd)))
+        (c2,rs1)    = purify (c1,rs ++ u1)
+        ors         = u `intersect` rs
+        purify :: ([DRSCon],[DRSRef]) -> ([DRSCon],[DRSRef])
+        purify ([],rs)             = ([],rs)
+        purify (c@(Rel _ d):cs,rs) = (c:ccs,rs1)
+          where (ccs,rs1) = purify (cs,rs ++ d)
+        purify (Neg d1:cs,rs)      = (Neg cd1:ccs,rs2)
+          where (cd1,rs1) = purifyRefs (d1,rs) gd
+                (ccs,rs2) = purify (cs,rs2)
+        purify (Imp d1 d2:cs,rs)   = (Imp cd1 cd2:ccs,rs3)
+          where (cd1,rs1) = purifyRefs (alphaConvertSubDRS d1 gd nrs,rs)  gd
+                (cd2,rs2) = purifyRefs (alphaConvertSubDRS d2 gd nrs,rs1) gd
+                (ccs,rs3) = purify (cs,rs2)
+                nrs = zip ors (newDRSRefs ors (drsVariables gd `union` drsVariables d))
+                ors = drsUniverse d1 `intersect` rs
+        purify (Or d1 d2:cs,rs)    = (Or cd1 cd2:ccs,rs3)
+          where (cd1,rs1) = purifyRefs (alphaConvertSubDRS d1 gd nrs,rs)  gd
+                (cd2,rs2) = purifyRefs (alphaConvertSubDRS d2 gd nrs,rs1) gd
+                (ccs,rs3) = purify (cs,rs2)
+                nrs = zip ors (newDRSRefs ors (drsVariables gd `union` drsVariables d))
+                ors = drsUniverse d1 `intersect` rs
+        purify (Prop r d1:cs,rs)   = (Prop r cd1:ccs,rs2)
+          where (cd1,rs1) = purifyRefs (d1,r:rs) gd
+                (ccs,rs2) = purify (cs,rs2)
+        purify (Diamond d1:cs,rs)  = (Diamond cd1:ccs,rs2)
+          where (cd1,rs1) = purifyRefs (d1,rs) gd
+                (ccs,rs2) = purify (cs,rs2)
+        purify (Box d1:cs,rs)      = (Box cd1:ccs,rs2)
+          where (cd1,rs1) = purifyRefs (d1,rs) gd
+                (ccs,rs2) = purify (cs,rs2)
--- src/Data/DRS/Merge.hs
+++ src/Data/DRS/Merge.hs
@@ -15,8 +15,6 @@ module Data.DRS.Merge
   drsMerge
 , (<<+>>)
 , drsResolveMerges
-, drsDisjoin
-, drsToCleanDRS
 ) where
 
 import Data.DRS.LambdaCalculus
@@ -26,19 +24,33 @@ import Data.DRS.Variables
 
 import Data.List (intersect, union)
 
--- | Merges DRS @d1@ with DRS @d2@
+---------------------------------------------------------------------------
+-- * Exported
+---------------------------------------------------------------------------
+
+---------------------------------------------------------------------------
+-- | Applies merge to 'DRS' @d1@ and 'DRS' @d2@.
+---------------------------------------------------------------------------
 drsMerge :: DRS -> DRS -> DRS
-drsMerge d1 d2 = merge rd1 (drsDisjoin rd1 rd2)
-  where rd1 = drsToCleanDRS $ drsResolveMerges d1
-        rd2 = drsToCleanDRS $ drsResolveMerges d2
+drsMerge d1 d2 = merge rd1 (rename rd1 (drsVariables rd2))
+  where rd1 = drsPurify $ drsResolveMerges d1
+        rd2 = drsPurify $ drsResolveMerges d2
         merge :: DRS -> DRS -> DRS
         merge (DRS u1 c1) (DRS u2 c2) = DRS (u1 `union` u2) (c1 `union` c2)
+        -- | Replace all occurrences of 'DRSRef's from @rs@ 
+        -- in 'DRS' @d@ by new 'DRSRef's.
+        rename :: DRS -> [DRSRef] -> DRS
+        rename d rs = drsAlphaConvert d2 (zip ors nrs)
+          where ors = drsVariables d `intersect` rs
+                nrs = newDRSRefs ors (drsVariables d `union` rs)
 
--- | Infix notation for 'drsMerge'
+-- | Infix notation for 'drsMerge'.
 (<<+>>) :: DRS -> DRS ->DRS
 d1 <<+>> d2 = d1 `drsMerge` d2
 
--- | Resolves all unresolved merges in a DRS
+---------------------------------------------------------------------------
+-- | Resolves all unresolved merges in a 'DRS'.
+---------------------------------------------------------------------------
 drsResolveMerges :: DRS -> DRS
 drsResolveMerges ld@(LambdaDRS _) = ld
 drsResolveMerges (Merge d1 d2)
@@ -54,50 +66,5 @@ drsResolveMerges (DRS u c)        = DRS u (map resolve c)
         resolve (Diamond d1) = Diamond (drsResolveMerges d1)
         resolve (Box d1)     = Box     (drsResolveMerges d1)
 
--- | Disjoins DRS @d2@ from DRS @d1@ by alpha converting overlapping bound 
--- referents to new referents
-drsDisjoin :: DRS -> DRS -> DRS
-drsDisjoin d1 d2 = drsAlphaConvert d2 (zip ors nrs)
-  where ors = drsVariables d1 `intersect` drsUniverses d2
-        nrs = newDRSRefs ors (drsUniverses d1 `union` drsUniverses d2)
 
-drsToCleanDRS :: DRS -> DRS
-drsToCleanDRS gdrs = fst $ cleanRefs (gdrs,[]) gdrs
 
-cleanRefs :: (DRS,[DRSRef]) -> DRS -> (DRS,[DRSRef])
-cleanRefs (ld@(LambdaDRS _),rs) _  = (ld,rs)
-cleanRefs (Merge d1 d2,rs)      gd = (Merge cd1 cd2,rs2)
-  where (cd1,rs1) = cleanRefs (d1,rs)  gd
-        (cd2,rs2) = cleanRefs (d2,rs1) gd
-cleanRefs (d@(DRS u _),rs)      gd = (DRS u1 c2,rs1)
-  where (DRS u1 c1) = drsAlphaConvert d (zip ors (newDRSRefs ors (drsVariables gd)))
-        (c2,rs1)    = clean (c1,rs ++ u1)
-        ors         = u `intersect` rs
-        clean :: ([DRSCon],[DRSRef]) -> ([DRSCon],[DRSRef])
-        clean ([],rs)             = ([],rs)
-        clean (c@(Rel _ d):cs,rs) = (c:ccs,rs1)
-          where (ccs,rs1) = clean (cs,rs ++ d)
-        clean (Neg d1:cs,rs)      = (Neg cd1:ccs,rs2)
-          where (cd1,rs1) = cleanRefs (d1,rs) gd
-                (ccs,rs2) = clean (cs,rs2)
-        clean (Imp d1 d2:cs,rs)   = (Imp cd1 cd2:ccs,rs3)
-          where (cd1,rs1) = cleanRefs (alphaConvertSubDRS d1 gd nrs,rs)  gd
-                (cd2,rs2) = cleanRefs (alphaConvertSubDRS d2 gd nrs,rs1) gd
-                (ccs,rs3) = clean (cs,rs2)
-                nrs = zip ors (newDRSRefs ors (drsVariables gd `union` drsVariables d))
-                ors = drsUniverse d1 `intersect` rs
-        clean (Or d1 d2:cs,rs)    = (Or cd1 cd2:ccs,rs3)
-          where (cd1,rs1) = cleanRefs (alphaConvertSubDRS d1 gd nrs,rs)  gd
-                (cd2,rs2) = cleanRefs (alphaConvertSubDRS d2 gd nrs,rs1) gd
-                (ccs,rs3) = clean (cs,rs2)
-                nrs = zip ors (newDRSRefs ors (drsVariables gd `union` drsVariables d))
-                ors = drsUniverse d1 `intersect` rs
-        clean (Prop r d1:cs,rs)   = (Prop r cd1:ccs,rs2)
-          where (cd1,rs1) = cleanRefs (d1,r:rs) gd
-                (ccs,rs2) = clean (cs,rs2)
-        clean (Diamond d1:cs,rs)  = (Diamond cd1:ccs,rs2)
-          where (cd1,rs1) = cleanRefs (d1,rs) gd
-                (ccs,rs2) = clean (cs,rs2)
-        clean (Box d1:cs,rs)      = (Box cd1:ccs,rs2)
-          where (cd1,rs1) = cleanRefs (d1,rs) gd
-                (ccs,rs2) = clean (cs,rs2)
