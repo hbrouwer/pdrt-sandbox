047b65e0c5099f153452a215421485de8ed5d3c9
Noortje Venhuizen <njvenhuizen@gmail.com>
2014-01-15 17:10:00 +0100

    added isPurePDRS and isProperPDRS

--- src/Data/PDRS/Properties.hs
+++ src/Data/PDRS/Properties.hs
@@ -12,7 +12,9 @@ PDRS properties
 
 module Data.PDRS.Properties 
 (
-  isPresupPDRS
+  isProperPDRS
+, isPurePDRS
+, isPresupPDRS
 , pdrsIsDifferentNP
 , pdrsIsSameNP
 ) where
@@ -29,6 +31,40 @@ import Data.PDRS.Variables
 ---------------------------------------------------------------------------
 -- * Exported
 ---------------------------------------------------------------------------
+
+---------------------------------------------------------------------------
+-- | Returns whether a 'PDRS' is /proper/, where:
+--
+-- [A 'PDRS' @p@ is proper /iff/]
+--
+--  * @p@ does not contain any free referents
+---------------------------------------------------------------------------
+
+isProperPDRS :: PDRS -> Bool
+isProperPDRS p = isProperSubPDRS p p
+  where isProperSubPDRS (LambdaPDRS _) _      = True
+        isProperSubPDRS (AMerge p1 p2) gp     = isProperSubPDRS p1 gp && isProperSubPDRS p2 gp
+        isProperSubPDRS (PMerge p1 p2) gp     = isProperSubPDRS p1 gp && isProperSubPDRS p2 gp
+        isProperSubPDRS lp@(PDRS _ _ _ cs) gp = all properCon cs
+          where properCon :: PCon -> Bool
+                properCon (PCon p (Rel _ d))    = all (\d' -> pdrsBoundPRef (PRef p d') lp gp) d
+                properCon (PCon _ (Neg p1))     = isProperSubPDRS p1 gp
+                properCon (PCon _ (Imp p1 p2))  = isProperSubPDRS p1 gp && isProperSubPDRS p2 gp
+                properCon (PCon _ (Or p1 p2))   = isProperSubPDRS p1 gp && isProperSubPDRS p2 gp
+                properCon (PCon p (Prop r p1))  = pdrsBoundPRef (PRef p r) lp gp && isProperSubPDRS p1 gp
+                properCon (PCon _ (Diamond p1)) = isProperSubPDRS p1 gp
+                properCon (PCon _ (Box p1))     = isProperSubPDRS p1 gp
+
+---------------------------------------------------------------------------
+-- | Returns whether a 'PDRS' is /pure/, where:
+--
+-- [A 'PDRS' @p@ is pure /iff/]
+--
+--  * @p@ does not contain any duplicate (otiose) variables
+---------------------------------------------------------------------------
+
+isPurePDRS :: PDRS -> Bool
+isPurePDRS p = p == pdrsPurify p
 
 ---------------------------------------------------------------------------
 -- | Returns whether a 'PDRS' is /presuppositional/, where:
